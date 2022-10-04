83750e634ba3513bb5031971e7b8c25dc00e1347
benjymansy123 <benjymansy123@gmail.com>
2022-10-04 16:35:07 -0400

    Add indexes for the other two.

--- data/sql/blizzlike/index.sql
+++ data/sql/blizzlike/index.sql
@@ -1,2 +1,4 @@
 INSERT INTO `mod_ptrtemplate_index` (`ID`, `Keywords`, `Level`, `MapAlliance`, `XAlliance`, `YAlliance`, `ZAlliance`, `OAlliance`, `MapHorde`, `XHorde`, `YHorde`, `ZHorde`, `OHorde`, `Enable`, `Comment`) VALUES
-(1, NULL, 60, 0, -8865.092773, 671.792603, 97.903442, 2.160984, 1, 1633.800171, -4440.467773, 15.634011, 3.083429, 0, 'Season of Mastery AQ40 Level 60');
+(1, NULL, 60, 0, -8865.092773, 671.792603, 97.903442, 2.160984, 1, 1633.800171, -4440.467773, 15.634011, 3.083429, 0, 'Season of Mastery AQ40 Level 60'),
+(2, NULL, 70, 0, -8414.122070, 1052.651367, 68.384247, 1.357533, 1, 1529.993286, -4412.640137, 12.976580, 6.278741, 0, 'Wrath Classic Level 70'),
+(3, NULL, 80, 571, 5852.440918, 692.975159, 642.039368, 0.856949, -1, 0, 0, 0, 0, 0, 'Wrath Classic Level 80');
