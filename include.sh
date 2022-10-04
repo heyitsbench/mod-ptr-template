1c06acee158284629954f69219c5fd610d69fb9d
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2022-10-04 14:36:48 -0400

    Update include.sh

--- include.sh
+++ include.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 ## GETS THE CURRENT MODULE ROOT DIRECTORY
-PTR_TEMPLATE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+PTR_TEMPLATE_PATH_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
-source $PTR_TEMPLATE_ROOT"/conf/conf.sh.dist"
+source $PTR_TEMPLATE_PATH_ROOT"/conf/conf.sh.dist"
 
-if [ -f $PTR_TEMPLATE_ROOT"/conf/conf.sh" ]; then
-    source $PTR_TEMPLATE_ROOT"/conf/conf.sh"
+if [ -f $PTR_TEMPLATE_PATH_ROOT"/conf/conf.sh" ]; then
+    source $PTR_TEMPLATE_PATH_ROOT"/conf/conf.sh"
 fi
