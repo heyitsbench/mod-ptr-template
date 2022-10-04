bb6fe0212f5c8fcba09fd9a0630dd6b3d1785148
benjymansy123 <benjymansy123@gmail.com>
2022-10-04 13:13:32 -0400

    Add reputation (Horde-only) and index entry for AQ40.

---
+++ data/sql/db-world/Blizzlike/index.sql
@@ -0,0 +1,2 @@
+INSERT INTO `mod_ptrtemplate_index` (`ID`, `Keywords`, `Level`, `MapAlliance`, `XAlliance`, `YAlliance`, `ZAlliance`, `OAlliance`, `MapHorde`, `XHorde`, `YHorde`, `ZHorde`, `OHorde`, `Enable`, `Comment`) VALUES
+(1, NULL, 60, 0, -8865.092773, 671.792603, 97.903442, 2.160984, 1, 1633.800171, -4440.467773, 15.634011, 3.083429, 0, 'Season of Mastery AQ40 Level 60');
