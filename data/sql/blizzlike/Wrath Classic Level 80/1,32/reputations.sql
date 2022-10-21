089b04c8a6fb445fbb9e306602db74eb3e6bd8e1
benjymansy123 <benjymansy123@gmail.com>
2022-10-20 23:55:38 -0400

    Add a couple Human Death Knight Level 80 queries.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/1,32/reputations.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(3, 1, 32, 1090, 0, NULL),
+(3, 1, 32, 1098, 3200, NULL),
+(3, 1, 32, 1037, 0, NULL),
+(3, 1, 32, 1094, 0, NULL),
+(3, 1, 32, 69, 3100, NULL),
+(3, 1, 32, 930, 3000, NULL),
+(3, 1, 32, 54, 3100, NULL),
+(3, 1, 32, 47, 3100, NULL),
+(3, 1, 32, 72, 4000, NULL);
