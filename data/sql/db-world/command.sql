db1cb145584f6ad87ff8dddf487c9e754443848c
benjymansy123 <benjymansy123@gmail.com>
2022-10-21 12:14:56 -0400

    Add command SQL.

---
+++ data/sql/db-world/command.sql
@@ -0,0 +1,5 @@
+INSERT INTO `command` (`name`, `security`, `help`) VALUES
+('template enable', 1, 'Syntax: .template enable $indexID'),
+('template disable', 1, 'Syntax: .template disable $indexID'),
+('template apply', 0, 'Syntax: .template apply $indexID'),
+('template list', 0, 'Syntax: .template list');
