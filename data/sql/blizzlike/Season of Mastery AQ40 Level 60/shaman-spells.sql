d25465ad4e5372b931794701fd122e9bbc4009cf
benjymansy123 <benjymansy123@gmail.com>
2022-10-25 19:54:35 -0400

    Upload more base spells up to 60.
    Hunter, Paladin, Priest.

---
+++ data/sql/blizzlike/Season of Mastery AQ40 Level 60/shaman-spells.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mod_ptrtemplate_spells` (`ID`, `RaceMask`, `ClassMask`, `SpellID`, `Comment`) VALUES
+(1, 0, 64, , ''),
+(1, 0, 64, , '');
---
+++ data/sql/blizzlike/Season of Mastery AQ40 Level 60/warlock-spells.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mod_ptrtemplate_spells` (`ID`, `RaceMask`, `ClassMask`, `SpellID`, `Comment`) VALUES
+(1, 0, 256, , ''),
+(1, 0, 256, , '');
