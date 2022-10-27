d536fcf51271dc4e53b799ab6767687bfce8ec1d
benjymansy123 <benjymansy123@gmail.com>
2022-10-27 16:08:42 -0400

    Add the rest of the actions for 60.

---
+++ data/sql/blizzlike/Season of Mastery AQ40 Level 60/hunter-action.sql
@@ -0,0 +1,12 @@
+INSERT INTO `mod_ptrtemplate_action` (`ID`, `RaceMask`, `ClassMask`, `Button`, `Action`, `Type`, `Comment`) VALUES
+(1, 174, 4, 1, 14287, 0, NULL),
+(1, 174, 4, 2, 75, 0, NULL),
+(1, 174, 4, 3, 14281, 0, NULL),
+(1, 174, 4, 4, 25295, 0, NULL),
+(1, 174, 4, 5, 1515, 0, NULL),
+(1, 174, 4, 6, 14325, 0, NULL),
+(1, 174, 4, 7, 2974, 0, NULL),
+(1, 174, 4, 8, 25296, 0, NULL),
+(1, 174, 4, 9, 13163, 0, NULL),
+(1, 174, 4, 10, 1205, 128, NULL),
+(1, 174, 4, 11, 4538, 128, NULL);
