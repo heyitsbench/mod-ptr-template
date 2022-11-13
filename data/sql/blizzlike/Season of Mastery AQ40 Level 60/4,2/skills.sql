1d7996fac020371507fffd469864b883715bbca1
benjymansy123 <benjymansy123@gmail.com>
2022-11-12 23:06:31 -0500

    Add some 60 skills.

---
+++ data/sql/blizzlike/Season of Mastery AQ40 Level 60/4,2/skills.sql
@@ -0,0 +1,12 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(1, 4, 2, 333, 300, 300, NULL),
+(1, 4, 2, 202, 300, 300, NULL),
+(1, 4, 2, 129, 300, 300, NULL),
+(1, 4, 2, 762, 150, 150, NULL),
+(1, 4, 2, 95, 300, 300, NULL),
+(1, 4, 2, 54, 300, 300, NULL),
+(1, 4, 2, 43, 300, 300, NULL),
+(1, 4, 2, 160, 300, 300, NULL),
+(1, 4, 2, 55, 300, 300, NULL),
+(1, 4, 2, 162, 300, 300, NULL),
+(1, 4, 2, 293, 1, 1, NULL);
