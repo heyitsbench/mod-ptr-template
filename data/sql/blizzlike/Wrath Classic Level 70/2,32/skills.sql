3079499420e67c7c622b7c2f87b6cd5f6fc9d868
benjymansy123 <benjymansy123@gmail.com>
2022-11-30 12:31:58 -0500

    Add some 70/80 skills.

---
+++ data/sql/blizzlike/Wrath Classic Level 70/2,32/skills.sql
@@ -0,0 +1,11 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(2, 2, 32, 129, 300, 300, NULL),
+(2, 2, 32, 762, 300, 300, NULL),
+(2, 2, 32, 44, 350, 350, NULL),
+(2, 2, 32, 95, 350, 350, NULL),
+(2, 2, 32, 229, 350, 350, NULL),
+(2, 2, 32, 43, 350, 350, NULL),
+(2, 2, 32, 172, 350, 350, NULL),
+(2, 2, 32, 55, 350, 350, NULL),
+(2, 2, 32, 162, 350, 350, NULL),
+(2, 2, 32, 293, 1, 1, NULL);
