474725b450a03e641125311f937e51bccb7286ad
benjymansy123 <benjymansy123@gmail.com>
2023-01-27 23:17:17 -0500

    Add human death knight 70 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 70/1,32/reputations.sql
@@ -0,0 +1,7 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(2, 1, 32, 1098, 3200, NULL),
+(2, 1, 32, 69, 3100, NULL),
+(2, 1, 32, 930, 3000, NULL),
+(2, 1, 32, 54, 3100, NULL),
+(2, 1, 32, 47, 3100, NULL),
+(2, 1, 32, 72, 4000, NULL);
