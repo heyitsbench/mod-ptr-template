3b18a405216d0011b7ed1145277778750c69a59c
benjymansy123 <benjymansy123@gmail.com>
2023-01-28 21:13:18 -0500

    Add a few 70 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 70/128,32/reputations.sql
@@ -0,0 +1,6 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(2, 128, 32, 530, 4000, NULL),
+(2, 128, 32, 76, 500, NULL),
+(2, 128, 32, 911, 3100, NULL),
+(2, 128, 32, 81, 500, NULL),
+(2, 128, 32, 68, 500, NULL);
