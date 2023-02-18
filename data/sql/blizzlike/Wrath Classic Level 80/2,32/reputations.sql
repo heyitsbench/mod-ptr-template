b808c36f9b43f8c06fac30abf781dea146dd831b
benjymansy123 <benjymansy123@gmail.com>
2023-02-17 20:36:37 -0500

    Add some orc 80 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/2,32/reputations.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(3, 2, 32, 1090, 0, NULL),
+(3, 2, 32, 1098, 3200, NULL),
+(3, 2, 32, 1052, 0, NULL),
+(3, 2, 32, 1124, 0, NULL),
+(3, 2, 32, 530, 500, NULL),
+(3, 2, 32, 76, 4000, NULL),
+(3, 2, 32, 911, 3100, NULL),
+(3, 2, 32, 81, 500, NULL),
+(3, 2, 32, 68, 500, NULL);
