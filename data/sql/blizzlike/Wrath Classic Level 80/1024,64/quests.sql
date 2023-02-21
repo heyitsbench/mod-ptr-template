ccbe9090d354157e23b00eb3b3b987eefbd640c4
benjymansy123 <benjymansy123@gmail.com>
2023-02-21 14:05:02 -0500

    Finish ally 80 quests.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/1024,64/quests.sql
@@ -0,0 +1,4 @@
+INSERT INTO `mod_ptrtemplate_quests` (`ID`, `RaceMask`, `ClassMask`, `QuestID`, `Comment`) VALUES
+(3, 1024, 64, 13807, NULL),
+(3, 1024, 64, 14100, NULL),
+(3, 1024, 64, 14111, NULL);
