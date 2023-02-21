4ba290570526037bb91b28a31289d5844cf70e45
benjymansy123 <benjymansy123@gmail.com>
2023-02-21 13:10:44 -0500

    Add some horde 80 quests.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/128,16/quests.sql
@@ -0,0 +1,12 @@
+INSERT INTO `mod_ptrtemplate_quests` (`ID`, `RaceMask`, `ClassMask`, `QuestID`, `Comment`) VALUES
+(3, 128, 16, 5644, NULL),
+(3, 128, 16, 5646, NULL),
+(3, 128, 16, 5658, NULL),
+(3, 128, 16, 5659, NULL),
+(3, 128, 16, 5660, NULL),
+(3, 128, 16, 5661, NULL),
+(3, 128, 16, 5662, NULL),
+(3, 128, 16, 5663, NULL),
+(3, 128, 16, 5679, NULL),
+(3, 128, 16, 12494, NULL),
+(3, 128, 16, 13807, NULL);
---
+++ data/sql/blizzlike/Wrath Classic Level 80/512,2/quests.sql
@@ -0,0 +1,2 @@
+INSERT INTO `mod_ptrtemplate_quests` (`ID`, `RaceMask`, `ClassMask`, `QuestID`, `Comment`) VALUES
+(3, 512, 2, 13807, NULL);
