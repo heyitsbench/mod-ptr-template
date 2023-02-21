1ff00bab6aa1254ce7bdef8ab96f7adf25b214d7
benjymansy123 <benjymansy123@gmail.com>
2023-02-21 15:45:53 -0500

    Condense 80 quests.

--- data/sql/blizzlike/Wrath Classic Level 80/1024,64/quests.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `mod_ptrtemplate_quests` (`ID`, `RaceMask`, `ClassMask`, `QuestID`, `Comment`) VALUES
-(3, 1024, 64, 13807, NULL),
-(3, 1024, 64, 14100, NULL),
-(3, 1024, 64, 14111, NULL);
