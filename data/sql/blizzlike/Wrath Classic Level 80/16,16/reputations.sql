43e1cf16f6f0e5bc8ab0a1a48330f3a5aa4c34b5
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-02-23 15:19:46 -0500

    Condense 80 reputations.

--- data/sql/blizzlike/Wrath Classic Level 80/16,16/reputations.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
-(3, 16, 16, 1090, 0, NULL),
-(3, 16, 16, 1052, 0, NULL),
-(3, 16, 16, 1124, 0, NULL),
-(3, 16, 16, 530, 500, NULL),
-(3, 16, 16, 76, 500, NULL),
-(3, 16, 16, 911, 3100, NULL),
-(3, 16, 16, 81, 500, NULL),
-(3, 16, 16, 68, 4000, NULL);
--- data/sql/blizzlike/Wrath Classic Level 80/512,16/reputations.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
-(3, 512, 16, 1090, 0, NULL),
-(3, 512, 16, 1037, 0, NULL),
-(3, 512, 16, 1094, 0, NULL),
-(3, 512, 16, 69, 500, NULL),
-(3, 512, 16, 930, 500, NULL),
-(3, 512, 16, 54, 4000, NULL),
-(3, 512, 16, 47, 500, NULL),
-(3, 512, 16, 72, 3100, NULL);
