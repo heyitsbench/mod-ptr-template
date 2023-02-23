62607cb54b61eeee1b811e3a8cfff432dafec525
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-02-23 13:31:43 -0500

    Add dwarf 80 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/4,16/reputations.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(3, 4, 16, 1090, 0, NULL),
+(3, 4, 16, 1091, 550, NULL),
+(3, 4, 16, 1037, 0, NULL),
+(3, 4, 16, 1094, 0, NULL),
+(3, 4, 16, 69, 3100, NULL),
+(3, 4, 16, 930, 3000, NULL),
+(3, 4, 16, 54, 3100, NULL),
+(3, 4, 16, 47, 4000, NULL),
+(3, 4, 16, 72, 3100, NULL);
