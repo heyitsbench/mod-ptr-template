2d206f9a653854e5f13840d8390936361599ad43
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-02-23 17:56:20 -0500

    Add undead 80 skills.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/16,16/skills.sql
@@ -0,0 +1,7 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(3, 16, 16, 762, 300, 300, NULL),
+(3, 16, 16, 95, 400, 400, NULL),
+(3, 16, 16, 54, 400, 400, NULL),
+(3, 16, 16, 136, 400, 400, NULL),
+(3, 16, 16, 162, 400, 400, NULL),
+(3, 16, 16, 228, 400, 400, NULL);
