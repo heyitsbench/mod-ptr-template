fde7705564a2857caaf34dcf6976c1716f7da418
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-02-23 14:20:23 -0500

    Add draenei 80 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/1024,32/reputations.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(3, 1024, 32, 1090, 0, NULL),
+(3, 1024, 32, 1098, 3200, NULL),
+(3, 1024, 32, 1037, 0, NULL),
+(3, 1024, 32, 1094, 0, NULL),
+(3, 1024, 32, 69, 3100, NULL),
+(3, 1024, 32, 930, 4000, NULL),
+(3, 1024, 32, 54, 3000, NULL),
+(3, 1024, 32, 47, 3100, NULL),
+(3, 1024, 32, 72, 3100, NULL);
