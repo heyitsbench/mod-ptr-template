72cb6ae7bc510fe472dabe9dc6d4ac999fc45222
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-02-23 14:57:20 -0500

    Add troll 80 reputations.

---
+++ data/sql/blizzlike/Wrath Classic Level 80/128,16/reputations.sql
@@ -0,0 +1,9 @@
+INSERT INTO `mod_ptrtemplate_reputations` (`ID`, `RaceMask`, `ClassMask`, `FactionID`, `Standing`, `Comment`) VALUES
+(3, 128, 16, 1090, 0, NULL),
+(3, 128, 16, 1037, 0, NULL),
+(3, 128, 16, 1094, 0, NULL),
+(3, 128, 16, 69, 4000, NULL),
+(3, 128, 16, 930, 500, NULL),
+(3, 128, 16, 54, 3100, NULL),
+(3, 128, 16, 47, 500, NULL),
+(3, 128, 16, 72, 500, NULL);
