2735c346de1698dbe58e130ea0f8cd28918686f2
benjymansy123 <benjymansy123@gmail.com>
2023-02-23 22:33:52 -0500

    Sort/comment/squish 80 skill queries.

--- data/sql/blizzlike/Wrath Classic Level 80/128,16/skills.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
-(3, 128, 16, 762, 300, 300, NULL),
-(3, 128, 16, 95, 400, 400, NULL),
-(3, 128, 16, 54, 400, 400, NULL),
-(3, 128, 16, 136, 400, 400, NULL),
-(3, 128, 16, 162, 400, 400, NULL),
-(3, 128, 16, 228, 400, 400, NULL);
