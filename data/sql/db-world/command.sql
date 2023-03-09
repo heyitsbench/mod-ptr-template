9d85ff532b191cf4cdbf94f007cc7bfb66fdef43
benjymansy123 <benjymansy123@gmail.com>
2023-03-08 22:33:40 -0500

    Add descriptions to commands.

--- data/sql/db-world/command.sql
+++ data/sql/db-world/command.sql
@@ -1,6 +1,6 @@
 DELETE FROM `command` WHERE `name` IN ('template apply', 'template disable', 'template enable', 'template list');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
-('template apply', 0, 'Syntax: .template apply $IndexID'),
-('template disable', 1, 'Syntax: .template disable $IndexID'),
-('template enable', 1, 'Syntax: .template enable $IndexID'),
-('template list', 0, 'Syntax: .template list');
+('template apply', 0, 'Syntax: .template apply $IndexID\nApplies the given template to your character.'),
+('template disable', 1, 'Syntax: .template disable $IndexID\nDisables the given template from being applied.'),
+('template enable', 1, 'Syntax: .template enable $IndexID\nEnables the given template to be applied.'),
+('template list', 0, 'Syntax: .template list\nList character template sets.');
