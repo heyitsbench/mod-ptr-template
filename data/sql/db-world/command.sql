ecf2f8e4f369984499ceef46f02f99bf115b3d00
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-03-23 23:04:35 -0400

    Adjust template enable/disable security level.

--- data/sql/db-world/command.sql
+++ data/sql/db-world/command.sql
@@ -1,6 +1,6 @@
 DELETE FROM `command` WHERE `name` IN ('template apply', 'template disable', 'template enable', 'template list');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('template apply', 0, 'Syntax: .template apply $IndexID\nApplies the given template to your character.'),
-('template disable', 1, 'Syntax: .template disable $IndexID\nDisables the given template from being applied.'),
-('template enable', 1, 'Syntax: .template enable $IndexID\nEnables the given template to be applied.'),
+('template disable', 3, 'Syntax: .template disable $IndexID\nDisables the given template from being applied.'),
+('template enable', 3, 'Syntax: .template enable $IndexID\nEnables the given template to be applied.'),
 ('template list', 0, 'Syntax: .template list\nList character template sets.');
