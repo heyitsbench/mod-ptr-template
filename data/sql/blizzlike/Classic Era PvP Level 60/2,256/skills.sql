0dd5fa2067550664a73b598b9eb6a65ae0b5c264
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-07-20 12:20:06 -0400

    Add warlock PvP 60 template skills.

---
+++ data/sql/blizzlike/Classic Era PvP Level 60/2,256/skills.sql
@@ -0,0 +1,11 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 2, 256, 333, 300, 300, 'Enchanting'),
+(6, 2, 256, 202, 300, 300, 'Engineering'),
+(6, 2, 256, 129, 300, 300, 'First Aid'),
+(6, 2, 256, 762, 150, 150, 'Riding'),
+(6, 2, 256, 173, 300, 300, 'Daggers'),
+(6, 2, 256, 95, 300, 300, 'Defense'),
+(6, 2, 256, 136, 300, 300, 'Staves'),
+(6, 2, 256, 43, 300, 300, 'Swords'),
+(6, 2, 256, 162, 300, 300, 'Unarmed'),
+(6, 2, 256, 228, 300, 300, 'Wands');
---
+++ data/sql/blizzlike/Classic Era PvP Level 60/64,256/skills.sql
@@ -0,0 +1,11 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 64, 256, 333, 300, 300, 'Enchanting'),
+(6, 64, 256, 202, 315, 315, 'Engineering'),
+(6, 64, 256, 129, 300, 300, 'First Aid'),
+(6, 64, 256, 762, 150, 150, 'Riding'),
+(6, 64, 256, 173, 300, 300, 'Daggers'),
+(6, 64, 256, 95, 300, 300, 'Defense'),
+(6, 64, 256, 136, 300, 300, 'Staves'),
+(6, 64, 256, 43, 300, 300, 'Swords'),
+(6, 64, 256, 162, 300, 300, 'Unarmed'),
+(6, 64, 256, 228, 300, 300, 'Wands');
