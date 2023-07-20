710ceb44b0be34e438a6ea9b600aa748644bb2f7
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-07-20 11:45:24 -0400

    Add rogue PvP 60 template skills.

---
+++ data/sql/blizzlike/Classic Era PvP Level 60/1,2/skills.sql
@@ -0,0 +1,12 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 1, 2, 333, 300, 300, 'Enchanting'),
+(6, 1, 2, 202, 300, 300, 'Engineering'),
+(6, 1, 2, 129, 300, 300, 'First Aid'),
+(6, 1, 2, 762, 150, 150, 'Riding'),
+(6, 1, 2, 95, 300, 300, 'Defense'),
+(6, 1, 2, 54, 300, 300, 'Maces'),
+(6, 1, 2, 43, 300, 300, 'Swords'),
+(6, 1, 2, 160, 300, 300, 'Two-Handed Maces'),
+(6, 1, 2, 55, 300, 300, 'Two-Handed Swords'),
+(6, 1, 2, 162, 300, 300, 'Unarmed'),
+(6, 1, 2, 293, 1, 1, 'Plate Mail');
---
+++ data/sql/blizzlike/Classic Era PvP Level 60/2,8/skills.sql
@@ -0,0 +1,13 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 2, 8, 633, 300, 300, 'Lockpicking'),
+(6, 2, 8, 333, 300, 300, 'Enchanting'),
+(6, 2, 8, 202, 300, 300, 'Engineering'),
+(6, 2, 8, 129, 300, 300, 'First Aid'),
+(6, 2, 8, 762, 150, 150, 'Riding'),
+(6, 2, 8, 45, 300, 300, 'Bows'),
+(6, 2, 8, 226, 300, 300, 'Crossbows'),
+(6, 2, 8, 173, 300, 300, 'Daggers'),
+(6, 2, 8, 95, 300, 300, 'Defense'),
+(6, 2, 8, 43, 300, 300, 'Swords'),
+(6, 2, 8, 176, 300, 300, 'Thrown'),
+(6, 2, 8, 162, 300, 300, 'Unarmed');
