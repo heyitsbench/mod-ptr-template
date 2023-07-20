4f4ab57eac4a9f51ece8a2f1ab14665819350df5
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-07-20 11:55:32 -0400

    Add priest PvP 60 template skills.

---
+++ data/sql/blizzlike/Classic Era PvP Level 60/16,16/skills.sql
@@ -0,0 +1,11 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 16, 16, 333, 300, 300, 'Enchanting'),
+(6, 16, 16, 202, 300, 300, 'Engineering'),
+(6, 16, 16, 129, 300, 300, 'First Aid'),
+(6, 16, 16, 762, 150, 150, 'Riding'),
+(6, 16, 16, 173, 300, 300, 'Daggers'),
+(6, 16, 16, 95, 300, 300, 'Defense'),
+(6, 16, 16, 54, 300, 300, 'Maces'),
+(6, 16, 16, 136, 300, 300, 'Staves'),
+(6, 16, 16, 162, 300, 300, 'Unarmed'),
+(6, 16, 16, 228, 300, 300, 'Wands');
