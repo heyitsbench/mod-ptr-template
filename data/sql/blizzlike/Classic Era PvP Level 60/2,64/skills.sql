74b10ab56f21207b29134901e2fca0359c6ee29c
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2023-07-20 11:59:56 -0400

    Add shaman PvP 60 template skills.

---
+++ data/sql/blizzlike/Classic Era PvP Level 60/2,64/skills.sql
@@ -0,0 +1,13 @@
+INSERT INTO `mod_ptrtemplate_skills` (`ID`, `RaceMask`, `ClassMask`, `SkillID`, `Value`, `Max`, `Comment`) VALUES
+(6, 2, 64, 333, 300, 300, 'Enchanting'),
+(6, 2, 64, 202, 300, 300, 'Engineering'),
+(6, 2, 64, 129, 300, 300, 'First Aid'),
+(6, 2, 64, 762, 150, 150, 'Riding'),
+(6, 2, 64, 44, 300, 300, 'Axes'),
+(6, 2, 64, 173, 300, 300, 'Daggers'),
+(6, 2, 64, 95, 300, 300, 'Defense'),
+(6, 2, 64, 54, 300, 300, 'Maces'),
+(6, 2, 64, 136, 300, 300, 'Staves'),
+(6, 2, 64, 172, 300, 300, 'Two-Handed Axes'),
+(6, 2, 64, 162, 300, 300, 'Unarmed'),
+(6, 2, 64, 413, 1, 1, 'Mail');
